57e2830ae7f6e18a1731d9b85af43f94c3d820e5
github-Goleva1 <golevaviktoria1@gmai.com>
2024-12-30 18:45:06 +0100

    init: add 5 solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const numStr = n.toString();
+  let max = 0;
+
+  for (let i = 0; i < numStr.length; i++) {
+    const tempNumber = parseInt(numStr.slice(0, i) + numStr.slice(i + 1), 10);
+    if (tempNumber > max) {
+      max = tempNumber;
+    }
+  }
+
+  return max;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const charCode = message.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const messageLetter = charCode - 65;
+        const keyLetter = key.charCodeAt(keyIndex % key.length) - 65;
+        const encryptedLetter = (messageLetter + keyLetter) % 26 + 65;
+        result += String.fromCharCode(encryptedLetter);
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const charCode = encryptedMessage.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        const encryptedLetter = charCode - 65;
+        const keyLetter = key.charCodeAt(keyIndex % key.length) - 65;
+        const decryptedLetter = (encryptedLetter - keyLetter + 26) % 26 + 65;
+        result += String.fromCharCode(decryptedLetter);
+        keyIndex++;
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
